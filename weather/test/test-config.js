2c3b8d0c9d6a0dc1dfe4fbb8328eb8bf738ee084
Alexandre Brulé <alexandre.brule@devinci.fr>
2016-02-23 15:50:45 +0100

    1st test in progress

--- weather/test/test-config.js
+++ weather/test/test-config.js
@@ -1,9 +1,12 @@
 var assert = require('assert'),
-    config = require('./../lib/config');
+    config = require('./../lib/config')
+    expect = require("chai").expect;
 
 
 describe('Config', function () {
-    it('has 3 props')
+    it('has 3 props', function() {
+    	expect(config.Config(units, ip).length).to.equal(3);
+    });
 
     it('after construction, 2 props are populated')
 
